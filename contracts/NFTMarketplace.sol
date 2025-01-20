06234cc8ad1a8745f89c7116f6c168c8fd988a88
oscarprdev <vlcoscar8@gmail.com>
2025-01-20 19:51:48 +0100

    Refactor smart contract to allow list offers

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -5,107 +5,145 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
-contract NFTMarketplace is ERC721URIStorage {
-    uint256 public currentId;
-    address owner;
-
-    address[] private NFTOwners;
+contract NFTCollection is ERC721URIStorage {
+    uint256 public NFTCount;
+    uint256 public offersCount;
+    address immutable owner;
+    uint256 constant FEE = 0.0015 ether;
 
     struct NFT {
-        uint256 id;
-        address owner;
-        string tokenURI;
-        uint price;
+        uint256 tokenId;       // Unique token ID
+        address creator;       // Creator of the NFT
+        address owner;         // Current owner of the NFT
+        string uri;            // Metadata URI
+        uint256 price;         // Price if listed for sale
+        bool isListed;         // Is it listed for sale
+        uint256 timestamp;     // Minting timestamp
     }
 
-    mapping(address => NFT[]) private nfts;
+    struct NFTOffer {
+        uint256 offerId;
+        uint256 tokenId;
+        address buyer;
+        uint256 price;
+    }
 
-    event NFTMinted(address indexed owner, string indexed tokenURI);
+    NFT[] public nftList;
+    NFTOffer[] public offersList;
+
+    event NFTMinted(address indexed owner, uint256 indexed tokenId);
     event NFTSelled(address indexed from, address indexed to, uint price);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
 
     constructor() ERC721("NFT Marketplace", "NFTM") {
         owner = msg.sender;
     }
 
-    function mintNFT(string memory _tokenURI, uint _price) external {
-         currentId++;
+    function mintNFT(string memory _uri, uint _price) external {
+         NFTCount++;
          NFT memory newNFT = NFT({
-            id: currentId,
+            tokenId: NFTCount,
+            creator: msg.sender,
             owner: msg.sender,
-            tokenURI: _tokenURI,
-            price: _price
+            uri: _uri,
+            price: _price,
+            isListed: false,
+            timestamp: block.timestamp
         });
 
-        if (!isNFTOwner(msg.sender)) {
-            NFTOwners.push(msg.sender);
-        }
+        nftList.push(newNFT);
 
-        nfts[msg.sender].push(newNFT);
+        _safeMint(msg.sender, NFTCount);
+        _setTokenURI(NFTCount, _uri);
 
-        _safeMint(msg.sender, currentId);
-        _setTokenURI(currentId, _tokenURI);
+        (bool success, ) = payable(address(this)).call{value: FEE}("");
+        require(success, "Mint payment fee failed");
 
-        emit NFTMinted(msg.sender, _tokenURI);
+        emit NFTMinted(msg.sender, NFTCount);
     }
 
-    function sellNFT(uint256 _id, address _to) external payable {
-        bool isOwner = isNFTOwner(msg.sender);
-        require(isOwner, "Only owner can sell NFT");
-
-        bool found = false;
-        uint index;
-
-        for (uint i = 0; i < nfts[msg.sender].length; i++) {
-            if (nfts[msg.sender][i].id == _id) {
-                found = true;
-                index = i; 
+    function acceptOffer(uint256 _offerId) external payable onlyOwner {
+        NFTOffer memory offer;
+        for (uint i = 0; i < offersList.length; i++) {
+            if (offersList[i].offerId == _offerId) {
+                offer = offersList[i];
                 break;
             }
         }
 
-        require(found, "NFT not found in sender's collection");
+        NFT memory nftToSell;
+        for (uint i = 0; i < nftList.length; i++) {
+            if (nftList[i].tokenId == offer.tokenId) {
+                nftToSell = nftList[i];
+                break;
+            }
+        }
 
-        NFT storage nftToSell = nfts[msg.sender][index];
-        require(nftToSell.price == msg.value, "Price must match the sent value");
+        require(nftToSell.price == offer.price, "Price must match the sent value");
+        require(nftToSell.isListed, "NFT must be listed for sale");
 
-        (bool success, ) = payable(msg.sender).call{value: msg.value}("");
+        (bool success, ) = payable(msg.sender).call{value: offer.price}("");
         require(success, "Payment transfer failed");
 
-        nftToSell.owner = _to;
+        nftToSell.owner = offer.buyer;
+        nftToSell.isListed = false;
 
-        emit NFTSelled(msg.sender, _to, msg.value);
+        emit NFTSelled(msg.sender, offer.buyer, offer.price);
     }
 
-    function listNFTs() external view returns (NFT[] memory) {
-        uint totalNFTs;
+    function createOffer(uint256 _tokenId) external payable {
+        offersCount++;
+        NFT memory nftToOffer;
 
-        for (uint i = 0; i < NFTOwners.length; i++) {
-            totalNFTs += nfts[NFTOwners[i]].length;
-        }
-
-        NFT[] memory allNFTs = new NFT[](totalNFTs);
-        uint currentIndex = 0;
-
-        for (uint i = 0; i < NFTOwners.length; i++) {
-            for (uint j = 0; j < nfts[NFTOwners[i]].length; j++) {
-                allNFTs[currentIndex] = nfts[NFTOwners[i]][j];
-                currentIndex++;
+         for (uint i = 0; i < nftList.length; i++) {
+            if (nftList[i].tokenId == _tokenId) {
+                nftToOffer = nftList[i];
+                break;
             }
         }
 
-        return allNFTs;
+        require(msg.value >= nftToOffer.price, "Price must be equal or greater than the current price");
+
+        offersList.push(NFTOffer(offersCount, nftToOffer.tokenId, msg.sender, msg.value));
+
+        (bool success, ) = payable(address(this)).call{value: msg.value}("");
+        require(success, "Offer failed");
     }
 
-    function listOwnedNFTs() external view returns (NFT[] memory) {
-        return nfts[msg.sender];
-    }
-
-    function isNFTOwner(address _owner) private view returns (bool) {
-        for (uint i = 0; i < NFTOwners.length; i++) {
-            if (NFTOwners[i] == _owner) {
-                return true;
+    function getNFTsByOwner(address _address) external view returns (NFT[] memory) {
+        uint itemCount;
+        for (uint i = 0; i < nftList.length; i++) {
+            if (nftList[i].owner == _address) {
+                itemCount++;
             }
         }
-        return false;
+
+        NFT[] memory nftsByOwner = new NFT[](itemCount);
+        uint index = 0;
+        for (uint i = 0; i < nftList.length; i++) {
+            if (nftList[i].owner == _address) {
+                nftsByOwner[index] = nftList[i];
+                index++;
+            }
+        }
+
+        return nftsByOwner;
+    }
+
+    function getNFTByTokenId(uint256 _tokenId) external view returns (NFT memory) {
+         NFT memory nft;
+
+        for (uint i = 0; i < nftList.length; i++) {
+            if (nftList[i].tokenId == _tokenId) {
+                nft = nftList[i];
+                break;
+            }
+        }
+
+        return nft;
     }
 }
